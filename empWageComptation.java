e4a8f6a68bb07cfdca59d9053c09f2d9cb9aa2a7
sravanichinna <sravanidane413.com>
2020-11-25 12:06:46 +0530

    Added: DailyEmployeeWage logiv

--- empWageComptation.java
+++ empWageComptation.java
@@ -9,11 +9,15 @@ public class empWageComptation{
                 int isPresent=1;
                 if(randomNum==isPresent)
                 {
-                        System.out.println("employee is present");
+                      System.out.println("employee is present");
+                        int empHrs=8;
+                        int ratePerHour=20;
+                        System.out.println("salary== "+(empHrs*ratePerHour));
                 }
                 else
                 {
                         System.out.println("employee is absent");
+                        System.out.println("salary==0");
                 }
 	}
 }
