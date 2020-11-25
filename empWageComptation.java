f94fe17a06e2547ab376bb96f60eda57024a39d7
sravanichinna <sravanidane413.com>
2020-11-25 12:34:44 +0530

    Added: TotalWorkingHoursOrDays logic

--- empWageComptation.java
+++ empWageComptation.java
@@ -6,34 +6,35 @@ public class empWageComptation{
 		Random random=new Random();
                 int randomNum=random.nextInt(3);
                 System.out.println("Random number: "+randomNum);
-                int fullTimePresent=2;
-                int partTimePresent=1;
-		int noOfDaysInMonth=20;
+                int maxHrsInMonth=100;
+		int noOfWorkDay=20;
 		int empRatePerHr=20;
+		int isPartTime=1;
+		int isFullTime=2;
 		int empHrs=0;
-		int empWage=0;
-		int empWageForMonth=0;
-		for(int day=1;day<=noOfDaysInMonth;day++)
-		{
-		switch(randomNum)
-		{
-		case partTimePresent:
-			System.out.println("part time ");
-			empHrs=4;
-			break;
-		case fullTimePresent:
-			System.out.println("fyll time ");
-			empHrs=8;
-			break;
-		default
-			System.out.println("employee is absent ");
-			empHrs=0;
-		}
-		empWage=(empHrs * empRatePerHr);
-		empWageForMonth=(empWageForMonth+empWage);
-		System.out.println("employee wage for day" + day + ":"+empWage);
-		}
-		System.out.println("employee wage of month is: "+empWageForMonth);
+		int totalEmpHrs=0;
+		int totalWorkDay=0;
+		int totalEmpWage=0;
+	while( totalWorkDay<noOfWorkDay && totalEmpHrs<=maxHrsInMonth)
+	{
+		 Random random=new Random();
+                int randomNum=random.nextInt(3);
+                System.out.println("Random number: "+randomNum);
+	switch(randomNum)
+	{
+	case isPartTime: empHrs=4;
+		break;
+	case isFullTime: empHrs=8;
+		break;
+	default; empHrs=0;
+	}
+		totalWorkDays++;
+		totalEmpHrs=totalEmpHrs + empHrs;
+		System.out.println("employee days: "+totalWorkDay + "work hours: "+totalEmpHrs);
+	}
+totalEmpWage=(toatalEmpHrs * empRatePerHour);
+System.out.println("employee total salary is: "+totalEmpWage);
+
 
 	}
 }
